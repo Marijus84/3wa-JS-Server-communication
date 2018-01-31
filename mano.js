c7a587de7dfa4b8f2edc72f3abaceeb43de6ccbd
Marijus84 <34712733+Marijus84@users.noreply.github.com>
2018-01-31 17:16:01 +0200

    Add files via upload

--- mano.js
+++ mano.js
@@ -1,13 +1,138 @@
-$(document).ready(function(){
+$(document).ready(function() {
+var info;
+var a;
+  $("#listbtn").click(list);
 
-  $(document).on("click", "#listbtn",  function(){
-    $.ajax ({
-      url:  "http://192.168.1.81:8080/list",
+  $(document).on("click", ".delete", function() {
+    a = info[this.id];
+    let b = JSON.stringify(a);
+    console.log(b);
+    $.ajax({
+      url: "http://192.168.1.81:8080/delete",
+      data: b,
+      contentType: "application/json",
+      type: "POST",
+      dataType: "json",
+      success: function(data) {
+        list();
+      }
+    });
+
+    return;
+  });
+
+
+  $(document).on("click", ".edit", function() {
+    a = info[this.id];
+    console.log(a.id);
+    console.log(a);
+    $("#newbtn").hide();
+    $("#listbtn").hide();
+    $(".list").show();
+    $("#submit").show();
+    $("#f").remove();
+    $("#editSubmit").remove();
+    $(".list").append("<form id=" + "f" + ">");
+    $(".tform").remove();
+    $("form").append("Username" + "</br>" + "<input type = " + " text " + "name = " + " userName " + " value = " + a.userName + " id= " + " vardas " + ">" + "</br>");
+    $("form").append("Email" + "</br>" + "<input type = " + " text " + " name =" + " eMail " + " value = " + a.eMail + " id = " + " eMail " + ">" + "</br>");
+    $("form").append("Age" + "</br>" + "<input type = " + " text " + " name = " + " age " + " value = " + a.age + " id = " + " age " + ">" + "</br>");
+    $(".list").append("</form>");
+    $(".list").append("<button type = " + "button" + " name = " + "submit" + " id = " + "editSubmit" + ">" + "Submit" + "</button>");
+    $(".list").append("<button type = 'button'  name = 'cansel' id = 'cancel'>" + "Cancel" + "</button>");
+
+    $("#cancel").click( function(){
+      $("#cancel").remove();
+      list();
+    });
+
+    $("#editSubmit").click( function() {
+      let temp = $("input#age").val();
+      old = parseInt(temp);
+      let str = {
+        id: a.id,
+        userName: $("input#vardas").val(),
+        eMail: $("input#eMail").val(),
+        age: old
+      };
+      let b = JSON.stringify(str);
+      console.log(b);
+      $.ajax({
+        url: "http://192.168.1.81:8080/update",
+        data: b,
+        contentType: "application/json",
+        type: "POST",
+        dataType: "json",
+        success: function(data) {
+          console.log(data);
+
+          list();
+        }
+      });
+      return;
+    });
+
+    return;
+  });
+
+
+  $("#newbtn").click(function() {
+    $("#newbtn").hide();
+    $("#listbtn").hide();
+    $(".list").append("<form id=" + "f" + ">");
+    $(".tform").remove();
+    $("#f").append("Username" + "</br>" + "<input type = " + " text " + "name = " + " userName " + "id= " + " vardas " + ">" + "</br>");
+    $("#f").append("Email" + "</br>" + "<input type = " + " text " + " name =" + " eMail " + " id = " + " eMail " + ">" + "</br>");
+    $("#f").append("Age" + "</br>" + "<input type = " + " text " + " name = " + " age " + "id = " + " age " + ">" + "</br>");
+    $(".list").append("<button type = " + "button" + " name = " + "submit" + " id = " + "submit" + ">" + "Submit" + "</button>");
+    $(".list").append("<button type = 'button'  name = 'cansel' id = 'cancel'>" + "Cancel" + "</button>");
+
+    $("#cancel").click( function(){
+      $("#cancel").remove();
+      list();
+    });
+
+    $("#submit").click(function() {
+      let temp = $("#age").val();
+      let old = parseInt(temp);
+      let str = {
+        userName: $("#vardas").val(),
+        eMail: $("#eMail").val(),
+        age: old
+      };
+      let b = JSON.stringify(str);
+
+      $.ajax({
+        url: "http://192.168.1.81:8080/add",
+        data: b,
+        contentType: "application/json",
+        type: "POST",
+        dataType: "json",
+        success: function(data) {
+          console.log(data);
+          list();
+        }
+      });
+    });
+  });
+
+
+  function list() {
+    $.ajax({
+      url: "http://192.168.1.81:8080/list",
       type: "GET",
-      success: function (data) {
+      success: function(data) {
+
+
+
+        info = data;
+        console.log(info);
+        $("#submit").remove();
+        $("#editSubmit").remove();
         $(".list").show();
-        $("form").hide();
-        $("#submit").hide();
+        $("form").remove();
+        $("#newbtn").show();
+        $("#listbtn").show();
         $(".tform").remove();
         $(".list").append("<table class ='tform'></table>");
         $("table").append("<tr>");
@@ -15,101 +140,38 @@ $(document).ready(function(){
         $("tr").append("<th>userName</th>");
         $("tr").append("<th>eMail</th>");
         $("tr").append("<th>age</th>");
-          for (i = 0; i < data.length; i++) {
-            tid = data[i].id;
-            tuser = data[i].userName;
-            tmail = data[i].eMail;
-            tage = data[i].age;
-            $(".tform").append("<tr id= " + i + " >" + "<td>" + tid + "</td>" + "<td>" + tuser + "</td>" + "<td>" + tmail + "</td>" + "<td>" + tage + "</td>"  + "<td>"
-            +"<button type = "+ "button" + " name = " + "delete" + " class = " + "delete"  + " id = " + i + ">"+ "Delete" + "</button>"  + "</td>"
-            + "<td>"
-            +"<button type = "+ "button" + " name = " + "edit" + " class = " + "edit"  + " id = " + i + ">"+ "Edit" + "</button>"  + "</td>"+"</tr>");
-            }
+        for (i = 0; i < data.length; i++) {
+          tid = data[i].id;
+          tuser = data[i].userName;
+          tmail = data[i].eMail;
+          tage = data[i].age;
+          console.log(tage);
+          console.log(data.length);
 
-          $(document).on("click", ".delete", function(){
-              a = data[this.id];
-              let b = JSON.stringify(a);
-              $.ajax ({
-                url:  "http://192.168.1.81:8080/delete",
-                data: b,
-                contentType: "application/json",
-                type: "POST",
-                dataType:"json",
-                success: function (data) {
-                  }
-                });
-              return;
-            });
+        //   $(".tform").append("<tr id= " + i + " >" + "<td>" + tid + "</td>" + "<td>" + tuser  + "</td>" + "<td>" + tmail  + "</td>" + "<td>" + tage + "</td>" + "<td>" +
+        //     "<button type = " + "button" + " name = " + "delete" + " class = " + "delete" + " id = " + i + ">" + "Delete" + "</button>" + "</td>" +
+        //     "<td>" +
+        //     "<button type = " + "button" + " name = " + "edit" + " class = " + "edit" + " id = " + i + ">" + "Edit" + "</button>" + "</td>" + "</tr>");
+        // //  $(".tform").append("<tr id= " + i + " >");
+          $(".tform").append("<tr id= " + i + " >");
+          $("tr#"+i).append("<td>");
+          $("td").last().append(document.createTextNode(tid));
+          $("tr#"+i).append("<td>");
+          $("td").last().append(document.createTextNode(tuser));
+          $("tr#"+i).append("<td>");
+          $("td").last().append(document.createTextNode(tmail));
+          $("tr#"+i).append("<td>");
+          $("td").last().append(document.createTextNode(tage));
+          $("tr#"+i).append("<td>");
+          $("td").last().append(document.createTextNode(tuser));
+          $("tr#"+i).append("<td>");
+          $("td").last().append("<button type = " + "button" + " name = " + "delete" + " class = " + "delete" + " id = " + i + ">" + "Delete" + "</button>");
+          $("tr#"+i).append("<td>");
+          $("td").last().append("<button type = " + "button" + " name = " + "edit" + " class = " + "edit" + " id = " + i + ">" + "Edit" + "</button>");
+        }
 
-          $(document).on("click", ".edit", function(){
-              a = data[this.id];
-              $("#newbtn").hide();
-              $("#listbtn").hide();
-              $(".list").show();
-              $("#submit").show();
-              $(".list").append("<form id=" + "f" + ">");
-              $(".tform").hide();
-              $("form").append("Username" + "</br>" + "<input type = " + " text "  + "name = " + " userName "+ " value = " +  a.userName  + " id= "+ " vardas "+ ">" + "</br>" );
-              $("form").append("Email" + "</br>" + "<input type = " + " text "  + " name =" + " eMail " + " value = " +  a.eMail + " id = "+ " eMail "+ ">" + "</br>");
-              $("form").append("Age" + "</br>" + "<input type = " + " text "  + " name = " + " age " + " value = " +  a.age + "id = "+ " age "+ ">" + "</br>");
-              $(".list").append("</form>");
-              $(".list").append("<button type = "+ "button" + " name = " + "submit" + " id = " + "submit" + ">"+ "Submit" + "</button>" );
-
-              let temp = $("input#age").val();
-              old = parseInt(temp);
-              let str =  {userName: $("input#vardas").val(), eMail:$("input#eMail").val(), age: old };
-              let b = JSON.stringify(str);
-
-              $.ajax ({
-                url:  "http://192.168.1.81:8080/edit",
-                data: b,
-                contentType: "application/json",
-                type: "POST",
-                dataType:"json",
-                success: function (data) {
-                 console.log(data);
-                }
-              })
-            return;
-          });
       }
     });
-  });
-
-    $("#newbtn").click(function(){
-      $("#newbtn").hide();
-      $("#listbtn").hide();
-      $(".list").append("<form id=" + "f" + ">");
-      $(".tform").hide();
-      $("form").append("Username" + "</br>" + "<input type = " + " text "  + "name = " + " userName " + "id= "+ " vardas "+ ">" + "</br>");
-      $("form").append("Email" + "</br>" + "<input type = " + " text "  + " name =" + " eMail " + " id = "+ " eMail "+ ">" + "</br>");
-      $("form").append("Age" + "</br>" + "<input type = " + " text "  + " name = " + " age " + "id = "+ " age "+ ">" + "</br>");
-      $(".list").append("</form>");
-      $(".list").append("<button type = "+ "button" + " name = " + "submit" + " id = " + "submit" + ">"+ "Submit" + "</button>" );
-    });
-
-    $(document).on("click", "#submit", function(){
-      let temp = $("input#age").val();
-      old = parseInt(temp);
-      let str =  {userName: $("input#vardas").val(), eMail:$("input#eMail").val(), age: old };
-      let b = JSON.stringify(str);
-
-      $.ajax ({
-        url:  "http://192.168.1.81:8080/add",
-        data: b,
-        contentType: "application/json",
-        type: "POST",
-        dataType:"json",
-        success: function (data) {
-          console.log(data);
-        }
-      });
-
-      $(".list").hide();
-      $("#newbtn").show();
-      $("#listbtn").show();
-      return;
-    });
-
-  return;
+    return;
+  }
 });
